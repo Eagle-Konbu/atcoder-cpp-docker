67e44dcfbdc97a37013438c780d5fb7c83565320
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-12-12 20:57:59 +0900

    Added ahc image.

---
+++ rust-ahc/template/main.rs
@@ -0,0 +1,35 @@
+use proconio::input;
+
+const TIMELIMIT: f64 = 1.8;
+
+fn main() {
+    let mut timer = Timer::new();
+}
+
+pub fn get_time() -> f64 {
+    let t = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap();
+    t.as_secs() as f64 + t.subsec_nanos() as f64 * 1e-9
+}
+
+struct Timer {
+    start_time: f64,
+}
+
+impl Timer {
+    fn new() -> Timer {
+        Timer {
+            start_time: get_time(),
+        }
+    }
+
+    fn get_time(&self) -> f64 {
+        get_time() - self.start_time
+    }
+
+    #[allow(dead_code)]
+    fn reset(&mut self) {
+        self.start_time = 0.0;
+    }
+}
