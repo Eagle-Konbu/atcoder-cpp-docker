851c82269afbb2ca63ff5b478ca740d7158ca341
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-02-06 14:17:31 +0900

    Added headers.

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 using namespace atcoder;
 typedef long long ll;
+typedef pair<ll, ll> P;
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 #define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
 #define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
 #define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(),x.end()),x.end())
+#define POSL(x, v) (lower_bound(x.begin(),x.end(),v)-x.begin())
+#define POSU(x, v) (upper_bound(x.begin(),x.end(),v)-x.begin())
+#define all(x) x.begin(), x.end()
+
 
 struct Edge {
     ll to;
