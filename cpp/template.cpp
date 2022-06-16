7b2955772229410d06dd4884d59e63595a135297
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-06-17 03:31:24 +0900

    Added typical functions.

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -9,9 +9,9 @@ typedef pair<ll, ll> P;
 #define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
 #define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
 #define SORT(x) sort(x.begin(), x.end())
-#define ERASE(x) x.erase(unique(x.begin(),x.end()),x.end())
-#define POSL(x, v) (lower_bound(x.begin(),x.end(),v)-x.begin())
-#define POSU(x, v) (upper_bound(x.begin(),x.end(),v)-x.begin())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
 #define all(x) x.begin(), x.end()
 
 template <typename T>
@@ -35,13 +35,91 @@ bool chmin(T &a, const T &b)
     return false;
 }
 
-struct Edge {
+ll factorial(ll n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+ll combination(ll a, ll b)
+{
+    return factorial(a) / (factorial(a - b) * factorial(b));
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return a / gcd(a, b) * b;
+}
+
+struct Edge
+{
     ll to;
     ll weight;
     Edge(ll to, ll weight) : to(to), weight(weight) {}
 };
 typedef vector<vector<ll>> Graph;
 typedef vector<vector<Edge>> WeightedGraph;
+
+vector<ll> dijkstra(const WeightedGraph &g, ll s)
+{
+    vector<ll> dis(g.size(), LONG_LONG_MAX);
+    dis[s] = 0;
+    priority_queue<P, vector<P>, greater<P>> pq;
+
+    pq.push({0, s});
+    while (!pq.empty())
+    {
+        auto [current_dis, v] = pq.top();
+        pq.pop();
+        if (dis[v] < current_dis)
+            continue;
+        for (auto e : g[v])
+        {
+            if (chmin(dis[e.to], current_dis + e.weight))
+            {
+                pq.push({dis[e.to], e.to});
+            }
+        }
+    }
+    return dis;
+}
+
+vector<ll> topo_sort(const Graph &g)
+{
+    vector<ll> res;
+    ll n = g.size();
+    vector<ll> in_deg(n);
+
+    rep(i, n) for (auto vv : g[i]) in_deg[vv]++;
+
+    queue<ll> que;
+    rep(i, n) if (in_deg[i] == 0) que.push(i);
+
+    while (!que.empty())
+    {
+        ll v = que.front();
+        res.push_back(v);
+        que.pop();
+        for (auto vv : g[v])
+        {
+            in_deg[vv]--;
+            if (in_deg[vv] == 0)
+                que.push(vv);
+        }
+    }
+
+    return res;
+}
 
 int main()
 {
