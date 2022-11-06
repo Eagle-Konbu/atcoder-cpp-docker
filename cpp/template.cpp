8297d446da29fbb9a4d44704af68027c2322eb58
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-11-07 01:38:33 +0900

    Added prime_factors to template.

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -112,6 +112,27 @@ vector<bool> eratosthenes(ll n)
     return is_prime;
 }
 
+vector<P> prime_factors(ll n)
+{
+    vector<P> res;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i != 0)
+            continue;
+        
+        ll cnt = 0;
+        while (n % i == 0)
+        {
+            cnt++;
+            n /= i;
+        }
+        res.push_back({i, cnt});
+    }
+    if (n != 1)
+        res.push_back({n, 1});
+    return res;
+}
+
 struct Edge
 {
     ll to;
